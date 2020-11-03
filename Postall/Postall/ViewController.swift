0f114c6d993b521d618d01e40a0408144bd15916
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-02 16:58:00 -1000

    Added logic to draw elements on the imageView representing the postcard.:

--- Postall/Postall/ViewController.swift
+++ Postall/Postall/ViewController.swift
@@ -11,6 +11,15 @@ class ViewController: UIViewController {
     
     // MARK: - Properties
     var colors = [UIColor]()
+    var image: UIImage?
+    
+    var topText = "Visit London"
+    var topFontName = "Helvetica Neue"
+    var topColor = UIColor.white
+    
+    var bottomText = "Home of Sherlock Holmes, Paddington Bear, and James Bond"
+    var bottomFontName = "Helvetica Neue"
+    var bottomColor = UIColor.white
     
     // MARK: - Outlets
     @IBOutlet var postcard: UIImageView!
@@ -20,6 +29,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         generateManyColors()
+        renderPostcard()
     }
     
     // MARK: - Methods
@@ -35,6 +45,44 @@ class ViewController: UIViewController {
                                     alpha: 1)
                 colors.append(color)
             }
+        }
+    }
+    
+    /// This method will draw the background image and two pieces of text on top. UIGraphicsImageRenderer will be used along with attributed strings so we can use colors, fonts, and text alignment.
+    private func renderPostcard() {
+        // Define the total drawing space
+        let drawRect = CGRect(x: 0, y: 0, width: 3000, height: 2400)
+        
+        // Define where to draw the top and bottom text
+        let topTextRect = CGRect(x: 250, y: 200, width: 2500, height: 800)
+        let bottomTextRect = CGRect(x: 250, y: 1800, width: 2500, height: 600)
+        
+        // Create UIFont instances out of the font names, providing fallbacks on failure
+        let topFont = UIFont(name: topFontName, size: 350) ?? UIFont.systemFont(ofSize: 250)
+        let bottomFont = UIFont(name: bottomFontName, size: 150) ?? UIFont.systemFont(ofSize: 100)
+        
+        // Create a centered paragraph style
+        let centered = NSMutableParagraphStyle()
+        centered.alignment = .center
+        
+        // Wrap that in attributed strings with the user's colors
+        let topTextAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: topColor, .font: topFont, .paragraphStyle: centered]
+        let bottomTextAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: bottomColor, .font: bottomFont, .paragraphStyle: centered]
+        
+        // Start rendering at the correct size
+        let renderer = UIGraphicsImageRenderer(size: drawRect.size)
+        
+        postcard.image = renderer.image { ctx in
+            // Fill the entire screen in gray
+            UIColor.gray.set()
+            ctx.fill(drawRect)
+            
+            // Draw the user's image at the top-left corner
+            image?.draw(at: CGPoint(x: 0, y: 0))
+            
+            // Draw the top and bottom text
+            topText.draw(in: topTextRect, withAttributes: topTextAttributes)
+            bottomText.draw(in: bottomTextRect, withAttributes: bottomTextAttributes)
         }
     }
 }
