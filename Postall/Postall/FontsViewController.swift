0eb7363efec8d3f8db98e26ea1c4c43756cfc656
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-02 14:26:35 -1000

    Set up Split View Controller and interface on ViewController.

---
+++ Postall/Postall/FontsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FontsViewController.swift
+//  Postall
+//
+//  Created by Jesse Ruiz on 11/2/20.
+//
+
+import UIKit
+
+class FontsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- Postall/Postall/ViewController.swift
+++ Postall/Postall/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Outlets
+    @IBOutlet var postcard: UIImageView!
+    @IBOutlet var colorSelection: UICollectionView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
