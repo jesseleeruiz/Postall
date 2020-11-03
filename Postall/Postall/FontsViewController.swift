55c58a93d20650043d8ce9da9411e65cafa6e075
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-02 14:47:14 -1000

    Added logic to showcase all fonts in the font family in the table view controller.

--- Postall/Postall/FontsViewController.swift
+++ Postall/Postall/FontsViewController.swift
@@ -8,9 +8,26 @@
 import UIKit
 
 class FontsViewController: UITableViewController {
-
+    
+    // MARK: - Properties
+    let fonts = UIFont.familyNames.sorted()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fonts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
+        let fontName = fonts[indexPath.row]
+        cell.textLabel?.text = fontName
+        cell.textLabel?.font = UIFont(name: fontName, size: 18)
+        return cell
     }
 }
